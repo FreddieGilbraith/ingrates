d107fb0bd99b5c4b19884c341e12d647102d4d84
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2020-07-23 15:57:04 +0100

    added spawnAsChild tests

---
+++ test/spawnAsChild.test.js
@@ -0,0 +1,6 @@
+describe("spawnAsChild", () => {
+	it.todo("lets to spawn an actor as the child of another");
+	it.todo("returns the id of the spawned actor");
+	it.todo("passes through spawn arguments");
+	it.todo("is accessable by the parent");
+});
