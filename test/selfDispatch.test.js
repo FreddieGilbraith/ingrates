95c7e03c0685acfd1cc7e686bdc5033f1b05c18f
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2020-08-05 13:03:44 +0100

    Added self dispatch tests

---
+++ test/selfDispatch.test.js
@@ -0,0 +1,5 @@
+describe("self dispatch", () => {
+	it.todo("can dispatch to itself");
+	it.todo("can re-dispatch the current msg to itself");
+	test.todo("self-re-dispatched msgs go to the back of the queue");
+});
