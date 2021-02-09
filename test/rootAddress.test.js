ac664ec651c528147c24997d04189f3f10d67034
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2021-02-09 11:12:25 +0000

    added 'root' test stub

---
+++ test/rootAddress.test.js
@@ -0,0 +1,9 @@
+import "babel-polyfill";
+
+import createActorSystem from "../src";
+
+describe("root actor address", () => {
+	it.todo(
+		"should always send messages addressed to 'root' to the root actor",
+	);
+});
