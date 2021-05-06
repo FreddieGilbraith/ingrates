c65461326d950a3c2945c60a75e6eeac97e5986d
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2021-05-06 23:26:59 +0100

    test async actors

---
+++ test/asyncActors.test.js
@@ -0,0 +1,51 @@
+import { pause, createTestSystem } from "./utils.js";
+
+async function SlowPokeActor({ dispatch, msg, state }) {
+	await pause();
+
+	const calls = (state ? state.calls : 0) + 1;
+
+	switch (msg.type) {
+		case "PING": {
+			dispatch(msg.src, { type: "PONG", calls });
+			break;
+		}
+	}
+
+	return {
+		...state,
+		calls,
+	};
+}
+
+const test = createTestSystem({ actors: [SlowPokeActor] });
+
+test(function RunAsyncActorTest({ self, spawn, dispatch, msg }, { t, done, fail }) {
+	switch (msg.type) {
+		case "START_TEST": {
+			t.plan(3);
+			const snail = spawn.snail(SlowPokeActor);
+			dispatch(snail, { type: "PING" });
+			dispatch(snail, { type: "PING" });
+			dispatch(snail, { type: "PING" });
+			break;
+		}
+
+		case "PONG": {
+			t.like(msg, { type: "PONG" });
+			if (msg.calls === 3) {
+				dispatch(self, { type: "DONE" });
+			}
+			break;
+		}
+
+		case "DONE": {
+			done();
+			break;
+		}
+
+		default: {
+			fail();
+		}
+	}
+});
